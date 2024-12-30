ba151e494f43b6bbd59357bae428d85adaa832a7
Syed Faraz Naqvi <syed.farazn.is@gmail.com>
2024-12-30 17:03:09 +0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -33,6 +33,7 @@ export default function Navbar({ onDemoClick }: NavbarProps) {
           {/* Desktop menu */}
           <div className="hidden md:block">
             <div className="flex items-center space-x-8">
+              <a href="#" className="text-gray-300 hover:text-white transition-colors">Home</a>
               <a href="#services" className="text-gray-300 hover:text-white transition-colors">Services</a>
               <a href="#portfolio" className="text-gray-300 hover:text-white transition-colors">Portfolio</a>
               {/* <a href="#about" className="text-gray-300 hover:text-white transition-colors">About</a> */}
